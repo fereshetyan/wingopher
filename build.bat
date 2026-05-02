8b8cea98ce7bd6543abfc1159005f59f9c2bb670
fereshetyan <fereshetyansimon@gmail.com>
2026-05-02 14:04:25 +0400

    build: remove UPX compression due to antivirus false positives

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @echo off
 echo Building WinGopher with UPX compression...
-wails build -platform windows/amd64 -upx -upxflags="--best --lzma"
+wails build -platform windows/amd64 -trimpath
 if %errorlevel% equ 0 (
     echo.
     echo Build complete! Binary: build\bin\wingopher.exe
